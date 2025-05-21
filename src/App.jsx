eb7c66d8af31b3129f09eb9650fecc4d4eb98669
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-21 06:56:32 +0530

    Added all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Domains from './components/Domains';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
-function App() {
+export default function App() {
 
   return (
-    <>
-    <Header/>
-      <main className='min-h-screen px-32 py-8 mb-4'>
-        <div>
-          <Hero/>
-        </div>
-      </main>
-    <Footer/>
-    </>
-  )
+    <div className="min-h-screen bg-gray-900 text-gray-100">
+      {/* Header */}
+      <Header />
+      {/* Hero Section */}
+      <Hero />
+      {/* DOMAINS SECTION */}
+      <Domains />
+      {/* PROJECTS SECTION */}
+      <Projects />
+      {/* ABOUT SECTION */}
+      <About />
+      {/* CONTACT SECTION */}
+      <Contact />
+      {/* FOOTER */}
+      <Footer />
+    </div>
+  );
 }
-
-export default App
